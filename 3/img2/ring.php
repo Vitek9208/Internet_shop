cb0645d2f76c31ade7bf95e13d93bc642cc0a8b4
Vitek <vitek9208@gmail.com>
2013-03-12 20:23:51 +0200

    12.03.2013 all allright

---
+++ 3/img2/ring.php
@@ -0,0 +1,238 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Наш сервис - НПП "КИАТОН" - промышленное оборудование -для птицефабрик, маркировочное</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<meta name="keywords" content="маркировка, маркировочное оборудование, продажа, Украина, маркировщик, датер, принтер-датер, датировщик, лазерная маркировка, лазерный маркиратор, принтер-датер,
+доставка, установка, расходные материалы, НПП КИАТОН, чернила для маркировщика, термотрансферный принтер, контактная маркировка, бесконтактная маркировка, Trei-P,  ЭКСТ, ЭКСТ-ДАТА 3М плюс, ЭКСТ-Дракон, ЭКСТ-Универсал,
+Smartlase, термотрансферная лента, оборудование для птицефабрик, сервис, доставка, установка, расходные материалы">
+<meta name="description" content="маркировка, маркировочное оборудование, продажа, Украина, маркировщик, датер, принтер-датер, датировщик, лазерная маркировка, лазерный маркиратор, принтер-датер,
+доставка, установка, расходные материалы, НПП КИАТОН, чернила для маркировщика, термотрансферный принтер, контактная маркировка, бесконтактная маркировка, Trei-P,  ЭКСТ, ЭКСТ-ДАТА 3М плюс, ЭКСТ-Дракон, ЭКСТ-Универсал,
+Smartlase, термотрансферная лента">
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body LEFTMARGIN=0 TOPMARGIN=0 MARGINWIDTH=0 MARGINHEIGHT=0 bgcolor="#4341F5" alink="#FFFFFF" vlink="#66FFFF" link="#FFFFFF">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" background="img2/2bg.jpg">
+  <tr> 
+    <td width="202" height="100"><a href="index.html"><img src="img2/2_01.jpg" border="0"></a></td>
+    <td width="157" height="100"><img src="img2/2_02.jpg"></td>
+    <td>Маркировочное оборудование, <br>
+      Оборудование для птицефабрик</td>
+    <td width="153"><a href="obor_staalkat.html"><img src="img2/2_03.jpg" border="0"></a></td>
+  </tr>
+</table>
+<table width="100%" height="21" cellpadding="0" cellspacing="0" border="0">
+  <tr> 
+  <tr> 
+    <td colspan="2"> <UL Id=Menu>
+        <LI> 
+          <H6 class="link4">О нас</H6>
+          <UL>
+            <LI><A Href="index2.html">Наше оборудование </A> 
+            <LI><A Href="index2.html">Наши парнеры</A> 
+            <LI><A Href="index2.html">Наши клиенты</A> 
+            <LI><A Href="index2.html">Наши контакты</A> 
+          </UL>
+        <LI> <a href="obor_mark.html"> 
+          <H6 class="link4">Маркировочное оборудование</H6>
+          </a> 
+          <UL>
+            <LI><A Href="obor_mark.html#2">Электрокаплеструйное</A> 
+            <LI><A Href="ekst.html ">Каплеструйное - техн.характ.</A> 
+            <LI><A Href="obor_mark.html#1">Контактное (термотрансферное)</A> 
+            <LI><A Href="contact.html">Контактное - техн.характ.</A> 
+            <LI><A Href="obor_mark.html#3">Крупносимвольное (на короба)</A> 
+            <LI><A Href="hugesymbol.html "> Крупносимвольное - техн.характ.</A> 
+            <LI><A Href=" obor_mark.html#4">Лазерное</A> 
+            <LI><A Href="laser.html">Лазерное - техн.характ.</A> 
+            <LI><A Href="rashod.html">Расходные материалы</A> 
+          </UL>
+        <LI> 
+          <H6 class="link4">Для птицефабрик - Staalkat</H6>
+          <UL>
+            <li><a href="obor_staalkat.html">О компании Staalkat</a></li>
+            <LI><A Href="obor_staalkat1.html">Машины для авт-й загрузки, сортировки 
+              и упаковки яиц </A> 
+            <LI><A Href="obor_staalkat2.html">Упаковщики яиц и укладчики лотков</A> 
+            <LI><A Href="obor_staalkat3.html">Оборудование для инкубаторов <br>
+              и другие дополнительные машины</A> 
+          </UL>
+        <LI> 
+          <H6 class="link4">Для птицеводства и жив-ва Cavenco</H6>
+          <UL>
+            <li><a href="cavenco.html">О компании Cavenco</a></li>
+            <LI><A Href="cavenco1.html#1">Авт-е линии кормления и поения для промышленной 
+              птицы</A> 
+            <LI><A Href="cavenco1.html#2 ">Системы вентиляции, обогрева, охлаждения</A> 
+            <LI><A Href="cavenco1.html#3">Микропроцессорные платы управления</A> 
+            <LI><A Href="cavenco1.html#4">Проекты &#8220;под ключ&#8221;</A> 
+            <LI><A Href="cavenco2.html#1"> Авт-е линии кормления и поения для 
+              свиней </A> 
+            <LI><A Href="cavenco2.html#2">Решетки, перегородки, подстилки, напольные 
+              решетки </A> 
+            <LI><A Href="cavenco2.html#3">Системы вентиляции, обогрева, охлаждения</A> 
+            <LI><A Href="cavenco2.html#4">Микропроцессорные платы управления</A> 
+          </UL>
+        <LI> 
+          <H6 class="link4">Статьи по птицеводству</H6>
+          <UL>
+            <LI><A Href="stat1.html">Родительское стадо</A> 
+            <LI><A Href="stat2.html">Обустройство птичников</A> 
+            <LI><A Href="stat3.html">Освещение в птичнике</A> 
+            <LI><A Href="stat4.htm">Производство пищевых яиц</A> 
+            <LI><A Href="stat5.htm">Выращивание бройлеров </A> 
+          </UL>
+        <LI> <a href="service.html"> 
+          <H6 class="link4">Сервис</H6>
+          </a> </li>
+        <li> <a href="polesnoe.html"> 
+          <H6 class="link4">Наши друзья</H6>
+          </a> 
+      </UL>
+      <Script Type="text/javascript">
+  var Menu=document.getElementById('Menu');
+  for(var i=0;i<Menu.childNodes.length;i++)
+    with(Menu.childNodes[i])
+      if(nodeName=='LI'){
+        onmouseover=function(){this.className='Hover';};
+        onmouseout=function(){this.className='';};
+      }//if
+</Script></td>
+  </tr>
+</table>
+<table cellpadding="0" cellspacing="0" width="100%" height="272">
+  <tr> 
+    <td width="200" height="270" valign="top" class="table2"> <h6>Если Вы хотите 
+        знать больше о промышленном оборудовании</h6>
+      - ДЛЯ МАРКИРОВКИ - заполните и перешлите нам форму техзадания, которая находится 
+      <a href="tehsadanie_na_mark.doc"><b>ЗДЕСЬ</b></a><br>
+      - ДЛЯ ПТИЦЕФАБРИК - для сортировки, упаковки, погрузки на лотки яиц, для 
+      инкубаторов - заполните и перешлите нам форму техзадания, которая находится<a href="staalkat.doc"> 
+      <b>ЗДЕСЬ</b></a><br>
+      - ДЛЯ ОСНАЩЕНИЯ ПТИЦЕФАБРИК системами вентиляции, кондиционирования, дозаторами 
+      медикаментов, системами для кормления молодняка бройлеров и ДЛЯ СВИНОФЕРМ 
+      - заполните и перешлите нам опросный лист, который находится <a href="cavenco.doc"><b>ЗДЕСЬ</b></a><br> 
+      <br>
+      Адреса наших почтовых ящиков <a href="mailto:ekstukraine@ukr.net"><b>ekstukraine@ukr.net<br>
+      </b></a> или <a href="mailto:ekstukraine@rambler.ru"><b><br>
+      ekstukraine@rambler.ru</b></a><br> </td>
+    <td class="table"><div align="right"><a href="polesnoe3_ua.html"> </a>&nbsp;&nbsp;&nbsp;&nbsp;</div>
+      <h2>&middot;&middot;&middot;&nbsp; Каталог ссылок:</h2>
+      <?
+$query_s = $_SERVER['QUERY_STRING'];
+$page = $_GET['page'];
+$serv = $_SERVER["HTTP_HOST"];
+
+$remote_url = '/glinks.php?serv='.$serv.'&page='.$page.'&query_s='.$query_s;
+$remote_server = "www.ringator.ru";
+$request = ''; // POST form
+
+$header = "POST $remote_url HTTP/1.0\n";
+$header .= "Host: $remote_server\n";
+$header .= "Content-Type: application/x-www-form-urlencoded\n";
+$header .= "Content-length: " . strlen($request) . "\n\n";
+
+$fp = fsockopen($remote_server, 80);
+if ($fp) {
+fputs($fp, $header . $request);
+$response = "";
+while (!feof($fp)) $response .= fgets($fp, 128);
+fclose($fp);
+echo substr($response, strpos($response, "\n\r\n"));
+}
+else {
+echo "<font color=red>cannot connect</font>
+\n";
+}
+?>
+      <h6> 
+        <div align="center">Дополнительная информация - в Харькове по тел. (057) 
+          752-80-20, 757-23-93;<br>
+          факс +38(057) 720-56-70; e-mail: <a href="mailto:ekstukraine@ukr.net"><b>ekstukraine@ukr.net</b></a><br>
+          Телефоны представительств в других городах <a href="index2.html#r"><b>здесь:</b></a></div>
+      </h6></td>
+  </tr>
+</table>
+<table width="100%" height="12" cellpadding="0" cellspacing="0" border="0" bgcolor="#1710C0">
+  <tr> 
+    <td height="21"  class="link">&nbsp;<a href="index2.html">О нас</a> | <a href="obor_mark.html" class="link2"> 
+      Маркировочное оборудование</a> | <a href="obor_staalkat.html" class="link2">Оборудование 
+      для птицефабрик</a> |<a href="rashod.html" class="link2"> Расходные материалы 
+      </a>| <a href="service.html" class="link2">Сервис</a> | <a href="obyav.html" class="link2">Доска 
+      объявлений</a> | <b>Наши друзья</b></td>
+  </tr>
+</table>
+<!--begin of Rambler's Top100 code -->
+<a href="http://top100.rambler.ru/top100/"> <img src="http://counter.rambler.ru/top100.cnt?648207" alt="счетчик" width=1 height=1 border=0></a> 
+<!--end of Top100 code-->
+<!--Rating@Mail.ru COUNTER-->
+<script language="JavaScript" type="text/javascript"><!--
+d=document;var a='';a+=';r='+escape(d.referrer)
+js=10//--></script>
+<script language="JavaScript1.1" type="text/javascript"><!--
+a+=';j='+navigator.javaEnabled()
+js=11//--></script>
+<script language="JavaScript1.2" type="text/javascript"><!--
+s=screen;a+=';s='+s.width+'*'+s.height
+a+=';d='+(s.colorDepth?s.colorDepth:s.pixelDepth)
+js=12//--></script>
+<script language="JavaScript1.3" type="text/javascript"><!--
+js=13//--></script>
+<script language="JavaScript" type="text/javascript"><!--
+d.write('<a href="http://top.mail.ru/jump?from=817177"'+
+' target=_top><img src="http://top.list.ru/counter'+
+'?id=817177;t=130;js='+js+a+';rand='+Math.random()+
+'" alt="Рейтинг@Mail.ru"'+' border=0 height=40 width=88/><\/a>')
+if(11<js)d.write('<'+'!-- ')//--></script>
+<noscript>
+<a
+target=_top href="http://top.mail.ru/jump?from=817177"><img
+src="http://top.list.ru/counter?js=na;id=817177;t=130"
+border=0 height=40 width=88
+alt="Рейтинг@Mail.ru"/></a> 
+</noscript>
+<script language="JavaScript" type="text/javascript"><!--
+if(11<js)d.write('--'+'>')//--></script>
+<!--/COUNTER-->
+<!-- SpyLOG f:0211 -->
+<script language="javascript"><!--
+Md=document;Mnv=navigator;Mp=1;
+Mn=(Mnv.appName.substring(0,2)=="Mi")?0:1;Mrn=Math.random();
+Mt=(new Date()).getTimezoneOffset();
+Mz="p="+Mp+"&rn="+Mrn+"&t="+Mt;
+Md.cookie="b=b";Mc=0;if(Md.cookie)Mc=1;Mz+='&c='+Mc;
+Msl="1.0";
+//--></script>
+<script language="javascript1.1"><!--
+Mpl="";Msl="1.1";Mj = (Mnv.javaEnabled()?"Y":"N");Mz+='&j='+Mj;
+//--></script>
+<script language="javascript1.2"><!-- 
+Msl="1.2";
+//--></script>
+<script language="javascript1.3"><!--
+Msl="1.3";//--></script>
+<script language="javascript"><!--
+Mu="u6129.18.spylog.com";My="";
+My+="<a href='http://"+Mu+"/cnt?cid=612918&f=3&p="+Mp+"&rn="+Mrn+"' target='_blank'>";
+My+="<img src='http://"+Mu+"/cnt?cid=612918&"+Mz+"&sl="+Msl+"&r="+escape(Md.referrer)+"&pg="+escape(window.location.href)+"' border=0 width=88 height=31 alt='SpyLOG'>";
+My+="</a>";Md.write(My);//--></script>
+<noscript>
+<a href="http://u6129.18.spylog.com/cnt?cid=612918&f=3&p=1" target="_blank"> <img src="http://u6129.18.spylog.com/cnt?cid=612918&p=1" alt='SpyLOG' border='0' width=88 height=31 > 
+</a> 
+</noscript>
+<!-- SpyLOG -->
+<center>
+  <script language="JavaScript"> var loc = ''; </script>
+  <script language="JavaScript1.4">try{ var loc = escape(top.location.href); }catch(e){;}</script>
+  <script language="JavaScript">
+var userid = 98202997; var page = 1;
+var rndnum = Math.round(Math.random() * 999111);
+document.write('<iframe src="http://ad3.bannerbank.ru/bb.cgi?cmd=ad&hreftarget=_blank&pubid=' + userid + '&pg=' + page + '&vbn=557&w=468&h=60&num=1&r=ssi&ssi=nofillers&r=ssi&nocache=' + rndnum + '&ref=' + escape(document.referrer) + '&loc=' + loc + '" frameborder=0 vspace=0 hspace=0 width=468 height=60 marginwidth=0 marginheight=0 scrolling=no>');
+document.write('<a href="http://ad3.bannerbank.ru/bb.cgi?cmd=go&pubid=' + userid + '&pg=' + page + '&vbn=557&num=1&w=468&h=60&nocache=' + rndnum + '&loc=' + loc + '&ref=' + escape(document.referrer) + '" target="_blank">');
+document.write('<img src="http://ad3.bannerbank.ru/bb.cgi?cmd=ad&pubid=' + userid + '&pg=' + page + '&vbn=557&num=1&w=468&h=60&nocache=' + rndnum + '&ref=' + escape(document.referrer) + '&loc=' + loc + '" width=468 height=60 Alt="   Аграрная Баннерная Сеть  " border=0></a></iframe>');
+</script>
+  <br>
+</center>
+
+</body>
+</html>
